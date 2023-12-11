1ba1ad25123033ae08aa60bdd38c87aaccf59b91
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2023-12-11 11:12:22 -0500

    Debugged runaway UseEffect and added an empty array to not have an infinite loop

--- src/data/bookData.js
+++ src/data/bookData.js
@@ -1,47 +1,50 @@
 export function generateBookData() {
-	const titles = [
-		'Journey Through Time',
-		'Mysteries of the Ocean',
-		'Mountains Beyond',
-		'Galaxies: Far and Beyond',
-		'Desert Tales',
-		'Life in Rainforests',
-		'Secrets of the Night Sky',
-		'Adventures Underground',
-		'Cultures of the World',
-		'Birds and Their Stories',
-	];
-	const authors = [
-		'Ava Thompson',
-		'Samuel Waters',
-		'Lucy Everest',
-		'Neil Cosmos',
-		'Sara Mirage',
-		'Jake Green',
-		'Luna Star',
-		'Terra Mole',
-		'Anna Globe',
-		'Faye Feather',
-	];
-	const dates = [
-		'2022-05-14',
-		'2018-09-21',
-		'2019-01-03',
-		'2020-11-27',
-		'2017-07-08',
-		'2021-03-16',
-		'2019-06-19',
-		'2020-02-25',
-		'2016-04-15',
-		'2018-10-09',
-	];
-	const pageCounts = [312, 487, 256, 529, 198, 402, 389, 315, 482, 276];
+  const titles = [
+    "Journey Through Time",
+    "Mysteries of the Ocean",
+    "Mountains Beyond",
+    "Galaxies: Far and Beyond",
+    "Desert Tales",
+    "Life in Rainforests",
+    "Secrets of the Night Sky",
+    "Adventures Underground",
+    "Cultures of the World",
+    "Birds and Their Stories",
+  ];
+  console.log(`book titles`, { titles });
+  const authors = [
+    "Ava Thompson",
+    "Samuel Waters",
+    "Lucy Everest",
+    "Neil Cosmos",
+    "Sara Mirage",
+    "Jake Green",
+    "Luna Star",
+    "Terra Mole",
+    "Anna Globe",
+    "Faye Feather",
+  ];
+  console.log(`Authors:`, { authors });
+  const dates = [
+    "2022-05-14",
+    "2018-09-21",
+    "2019-01-03",
+    "2020-11-27",
+    "2017-07-08",
+    "2021-03-16",
+    "2019-06-19",
+    "2020-02-25",
+    "2016-04-15",
+    "2018-10-09",
+  ];
+  console.log(`dates:`, { dates });
+  const pageCounts = [312, 487, 256, 529, 198, 402, 389, 315, 482, 276];
 
-	return titles.map((title, index) => ({
-		id: index + 1,
-		title: title,
-		author: authors[index],
-		datePublished: dates[index],
-		pages: pageCounts[index],
-	}));
+  return titles.map((title, index) => ({
+    id: index + 1,
+    title: title,
+    author: authors[index],
+    datePublished: dates[index],
+    pages: pageCounts[index],
+  }));
 }
